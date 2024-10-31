cecb84be1a051e130237799402ffd7f7352288b9
zigacek <zigacek@D251-UC1>
2024-10-31 16:27:26 +0100

    Přidejte soubory projektu.

---
+++ TridyAObjekty_kostka/Kostka.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TridyAObjekty_kostka
+{
+    public class Kostka
+    {
+        public int[] statisticaHodu;
+        private static int pocetStranKostky = 6;
+        private double prumerHozenychHodnot = 0;
+        private int pocetHoduCelkem = 0;
+        private int celkoveHozeno = 0;
+        Random random; //random by mel byt inicializovan jednou. nejlepe uvnitr konstruktoru
+        public Kostka() { //Specialni metoda nazvana konstruktor, umoznuje inicializaci promennych i tech private
+            pocetStranKostky = 6;
+            statisticaHodu = new int[6];
+            random = new Random();
+        }
+        public Kostka(int argPocetStran)
+        {
+            pocetStranKostky = argPocetStran;
+            statisticaHodu = new int[argPocetStran];
+            random = new Random();
+        }
+        public int HodKostkou()
+        {
+            //Trida pro generovani nahodnych cisel
+            //random = new Random();
+            int jedenHod = 0;
+            jedenHod = random.Next(1, pocetStranKostky + 1);
+            statisticaHodu[jedenHod - 1]++;
+            pocetHoduCelkem++;
+            celkoveHozeno += jedenHod;
+            prumerHozenychHodnot  = (double)celkoveHozeno/pocetHoduCelkem;
+            return jedenHod;
+        }
+        public string InformaceOKostce()
+        {
+            //return "" + pocetStranKostky;
+            return pocetStranKostky.ToString();
+        }
+        public string PoctyHozenychCiselText()
+        {
+            string statistInfo = "";
+            for (int i = 0; i < statisticaHodu.Length; i++)
+            {
+                statistInfo += "CISLO " + (i + 1) + " HOZENO " + statisticaHodu[i] + "x" + "\n";
+            }
+            return statistInfo;
+
+        }
+        public double GetPrumerHozenychHodnot()
+        {
+            return prumerHozenychHodnot;
+        }
+        
+
+    }
+}
---
+++ TridyAObjekty_kostka/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.Design;
+
+namespace TridyAObjekty_kostka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Kostka mojeKostka = new Kostka(12);
+            for (int i = 0; i < 1000; i++)
+            {
+                int jedenHod = mojeKostka.HodKostkou();
+                Console.WriteLine(jedenHod);
+            }
+            Console.WriteLine(mojeKostka.PoctyHozenychCiselText());
+            Console.WriteLine(mojeKostka.GetPrumerHozenychHodnot());
+            //vytvorit kostku jako objekt s urcitym poctem stran
+
+        }
+
+        //int padla1 = 0;
+        //int padla2 = 0; 
+
+       
+
+
+        
+
+
+
+
+        //pocet stran = 6 1-6
+        
+        //pocet kostek
+        
+        //Metoda na generovani Hodu
+
+        //Statistika hodu - pocet hozenych cisel, prumer hodu
+
+        //Vytiskni informace o kostce
+
+
+        
+
+
+    }
+}
